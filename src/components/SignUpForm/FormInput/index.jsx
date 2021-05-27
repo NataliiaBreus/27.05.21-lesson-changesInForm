8d5c9edfa09607b0cc9ac9737cbdc864d8299b97
Nataliia Breus <breusnataly@gmail.com>
2021-05-27 12:05:17 +0300

    commit

---
+++ src/components/SignUpForm/FormInput/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import cx from 'classnames';
+import style from "./FormInput.module.css";
+
+export default function FormInput(props) {
+  const {name, value, onChange = () => {}, validationHandler = () =>{}, placeholder, type = "text"} = props;
+  const isValid = validationHandler(value);
+  return (
+    <input
+    name = {name}
+      value={value}
+      onChange={onChange}
+      className={cx(
+        style.input,
+        { [style.invalidInput]: !isValid },
+        { [style.validInput]: isValid }
+      )}
+      type={type}
+      placeholder={placeholder}
+    />
+  );
+}
--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -1,64 +1,65 @@
 import React, { Component } from "react";
+import cx from "classnames";
 import style from "./SignUpForm.module.css";
+import FormInput from "./FormInput";
 
-const initialValues = {
-    firstname: '',
-    lastname: '',
-    email: '',
-    password: '',
-}
+const intialValues = {
+  firstname: "",
+  lastname: "",
+  email: "",
+  password: "",
+  
+};
+
 class SignUpForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {...initialValues}
-    }
+  constructor(props) {
+    super(props);
+    this.state = { ...intialValues };
+  }
+
   submitHandler = (event) => {
-      const{registerUser} = this.props;
+    const { registerUser } = this.props;
     event.preventDefault();
-    console.log(event);
-    registerUser(this.state);
-    this.setState({...initialValues});
+    registerUser({ ...this.state });
+    this.setState({ ...intialValues });
   };
 
-  handleChange = ({target:{name, value}}) =>{
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value, [`is${name}Valid`]: !value.includes(" ") });
+  };
+
+  firstnameChange(){
+
+  }
+
+  lastnameChange(){
+
+  }
+  emailChange(){
+    
+  }
+  passwordChange({ target: { value } }){
+    const isValid = value.length >= 8;
     this.setState({[name]: value});
   }
+
   render() {
-      const{firstname, lastname, email, password} = this.state;
+    const {
+      firstname,
+      lastname,
+      email,
+      password,
+      isemailValid,
+      isfirstnameValid,
+      islastnameValid,
+      ispasswordValid,
+    } = this.state;
     return (
       <form className={style.container} onSubmit={this.submitHandler}>
-        <input
-        value={firstname}
-          className={style.input}
-          type="text"
-          name="firstname"
-          onChange = {this.handleChange}
-          placeholder="Введите имя"
-        />
-        <input
-        value={lastname}
-          className={style.input}
-          type="text"
-          name="lastname"
-          onChange = {this.handleChange}
-          placeholder="Введите фамилию"
-        />
-        <input
-        value={email}
-          className={style.input}
-          type="email"
-          name="email"
-          onChange = {this.handleChange}
-          placeholder="Введите email"
-        />
-        <input
-        value = {password}
-          className={style.input}
-          type="password"
-          name="password"
-        onChange = {this.handleChange}
-          placeholder="Введите пароль"
-        />
+        <FormInput name = "firstname" value={firstname} onChange={this.handleChange} validationHandler={this.validateFirstname} placeholder="Введите имя"/>
+        <FormInput name = "lastname" value={lastname} onChange={this.handleChange} validationHandler={this.validateLastname} placeholder="Введите фамилию"/>
+        <FormInput name = "email" value={email} onChange={this.handleChange} validationHandler={this.validateEmail} placeholder="Введите email" type="email"/>
+        <FormInput name = "password" value={password} onChange={this.handleChange} validationHandler={this.validatePassword} placeholder="Введите пароль" type="password"/>
         <input className={style.input} type="submit" />
       </form>
     );
@@ -66,3 +67,8 @@ class SignUpForm extends Component {
 }
 
 export default SignUpForm;
+
+/*
+  1. Написать функции-обработчики изменений имени, фамилии, email.
+  2. Добавить валидацию этих полей.
+*/
